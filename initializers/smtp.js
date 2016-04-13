82e693dec95d340eafc2dd37af57e08e28a6bd17
Evan Tahler <evantahler@gmail.com>
2016-04-13 16:17:02 -0700

    commas

--- initializers/smtp.js
+++ initializers/smtp.js
@@ -6,8 +6,8 @@ module.exports = {
 
   initialize: function(api, next){
     var transport = {
-      name: 'smtp'
-      description: 'for sending E-Mail via SMTP'
+      name: 'smtp',
+      description: 'for sending E-Mail via SMTP',
 
       requiredDataKeys: {
         person: ['email']
