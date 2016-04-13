4123ca6feb2296755bd70e8a90fb888be156ef7c
Evan Tahler <evantahler@gmail.com>
2016-04-13 16:12:21 -0700

    basics

---
+++ initializers/smtp.js
@@ -0,0 +1,34 @@
+var nodemailer    = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+module.exports = {
+  loadPriority:  1000,
+
+  initialize: function(api, next){
+    var transport = {
+      name: 'smtp'
+      description: 'for sending E-Mail via SMTP'
+
+      requiredDataKeys: {
+        person: ['email']
+      },
+
+      deliver: function(payload, person, callback){
+        var email = {
+          to:      person.data.email,
+          from:    payload.from,
+          subject: payload.subject,
+          html:    payload.body,
+        };
+
+        api.smtp.client.sendMail(email, callback);
+      }
+
+      client: nodemailer.createTransport(smtpTransport(api.config.smtp));
+    }
+
+    api.trasnports.push(transport);
+
+    next();
+  },
+};
